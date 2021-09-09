2cfc51e4e0f95a5a595222e83abc57e979c9ad10
Samk13 <samarbid13@gmail.com>
2021-09-09 22:48:50 +0200

    classmethods

--- oop.py
+++ oop.py
@@ -1,3 +1,6 @@
+import csv
+
+
 class Item:
     rate = 19.0
     all = []
@@ -7,20 +10,33 @@ class Item:
         assert price >= 0, "Price must be >= 0"
         assert quantity >= 0, "Quantity must be >= 0"
         assert isinstance(name, str), "Name must be a string"
+
+        # initialize instance variables
         self.name = name
         self.price = price
         self.quantity = quantity
+
+        # append each item initiated to the list
         Item.all.append(self)
 
+    # representation of the object
     def __repr__(self):
         return f"Item({self.name}, {self.price}, {self.quantity})"
 
+    # normal function
     def calculate_total_price(self):
         return float(self.price) * float(self.quantity)
 
+    # class method to read the csv file and return a list of items
+    @classmethod
+    def initiate_from_csv(cls, filename: str):
+        with open(filename, "r") as f:
+            reader = csv.DictReader(f)
+            items = list(reader)
+        for item in items:
+            Item(name=item["name"], price=float(item["price"]),
+                 quantity=int(item["quantity"]))
 
-item1 = Item("apple", 12)
-item2 = Item("samsung", 13)
-item3 = Item("Huawei", 15)
 
+Item.initiate_from_csv("items.csv")
 print(Item.all)
